f0df6cdd83aa79271de407ee10a9bbe339724b4b
spikewuller <spikewuller@gmail.com>
2022-09-30 08:18:14 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.all('/asd', (req, res) => {
+    res.status(200).send(process.env.TEST);
 })
 app.listen(process.env.PORT || 3000)
